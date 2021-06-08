f71b04a188958e6a3eb4a3759a53c8d35227db01
Eirik Gallefoss <eirik@kakepepper.org>
2021-06-08 09:52:59 +0200

    Allow cors

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 let notes = [{ id: 1, content: "HTML is easy", date: "2019-05-30T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2019-05-30T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2019-05-30T19:20:14.298Z", important: true }]
