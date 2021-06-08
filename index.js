ab820a8dd5e62e07bd1b83c87167220b21452d28
Eirik Gallefoss <eirik@kakepepper.org>
2021-06-08 12:49:13 +0200

    Add database connection

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-let notes = [{ id: 1, content: "HTML is easy", date: "2019-05-30T17:30:31.098Z", important: true }, { id: 2, content: "Browser can execute only Javascript", date: "2019-05-30T18:39:34.091Z", important: false }, { id: 3, content: "GET and POST are the most important methods of HTTP protocol", date: "2019-05-30T19:20:14.298Z", important: true }]
+const errorHandler = (error, response, request, next) => {
+    console.log(error)
+    next()
+}
 
+app.use(errorHandler)
 
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/api/notes', (request, response, next) => {
+    Note.find({})
+        .then(notes => {
+            response.json(notes)
+        })
+        .catch(error => next(error))
 })
 
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
-
-const generateId = () => {
-    const maxId = notes.length > 0 ?
-        Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if (!body.content) {
@@ -47,23 +49,26 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+        date: new Date()
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
